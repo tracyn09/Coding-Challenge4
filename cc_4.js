f6d3399bc44354d3639dbd02fa24a97dea09756c
tracyn09 <tracyn0099@gmail.com>
2025-01-29 12:05:07 -0500

    Task 1– Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,13 @@
+// Task 1
+
+// Declare purchaseAmount With a Value
+var purchaseAmount= 679
+
+// Use IF Statement to Apply Discount
+let finalAmount;
+if (purchaseAmount > 100) {
+    finalAmount=purchaseAmount * .3;}
+else { finalAmount=purchaseAmount}
+
+// Updated Final Value
+console.log(`Final amount after discount: $${finalAmount}`);
