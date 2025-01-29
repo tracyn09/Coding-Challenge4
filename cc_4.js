50c4abf9b5454f6d4570d170aea6cce56d139615
tracyn09 <tracyn0099@gmail.com>
2025-01-29 12:08:03 -0500

    Task 2– Sales Report

--- cc_4.js
+++ cc_4.js
@@ -11,3 +11,16 @@ else { finalAmount=purchaseAmount}
 
 // Updated Final Value
 console.log(`Final amount after discount: $${finalAmount}`);
+
+// Task 2
+
+// Declare Sales Array
+let sales = [457,689,232,156,743]
+let sum = 0 // To Start Adding from 0
+
+// Use Loop to Calculate Total Sales
+for(let x=0; x<sales.length; x++)
+{sum += sales[x]};
+
+// Update Total Sales
+console.log(`Total Sales: ${sum}`);
